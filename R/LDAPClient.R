5b6b8c0520d279880c9e15e8d7ae0a95fea5f32b
eblondel <emmanuel.blondel1@gmail.com>
2019-10-29 15:48:32 +0100

    add logger

--- R/LDAPClient.R
+++ R/LDAPClient.R
@@ -19,8 +19,9 @@
 #'
 #' @section Methods:
 #' \describe{
-#'  \item{\code{new(hostname, port, dc)}}{
-#'    This method is used to instantiate a LDAP client.
+#'  \item{\code{new(hostname, port, dc, logger)}}{
+#'    This method is used to instantiate a LDAP client.  The logger can be either
+#'    NULL, "INFO" (with minimum logs), or "DEBUG" (for complete curl calls logs)
 #'  }
 #'  \item{\code{getPersons(pretty)}}{
 #'    Retrieves a list of persons. By default, this is returned as \code{data.frame}.
@@ -30,28 +31,35 @@
 #' 
 #' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
 LDAPClient <- R6Class("LDAPClient",
+  inherit = ldap4RLogger,
   private = list(),
   public = list(
     root_url = NULL,
     root = NULL,
     dc = NULL,
     url = NULL,
-    initialize = function(hostname, port = NULL, dc){
+    initialize = function(hostname, port = NULL, dc, logger = NULL){
+      super$initialize(logger = logger)
       if(!is.null(port)) hostname <- paste0(hostname, ":", port)
       dcurl <- paste0(lapply(unlist(strsplit(dc,"\\.")), function(x){paste0("dc=",x)}),collapse=",")
       self$root_url <- sprintf("ldap://%s", hostname)
+      self$INFO(sprintf("LDAP root URL: %s", self$root_url))
       self$root <- RCurl::getURL(self$root_url)
+      self$INFO(sprintf("LDAP root: %s", self$root))
       message(self$root)
       self$dc <- dcurl
       self$url <- sprintf("ldap://%s/%s", hostname, dcurl)
+      self$INFO(sprintf("LDAP main URL: %s", self$url))
       
     },
     
     #getPersons
     getPersons = function(pretty = TRUE){
       people_attrs <- c("uid", "cn","sn","givenName", "mail", "title", "ou", "employeeType", "objectClass")
-      ldap_person <- RCurl::getURL(sprintf("%s?%s?sub?(objectClass=person)", self$url, paste0(people_attrs,collapse=",")))
-      print(ldap_person)
+      ldap_person_url <- sprintf("%s?%s?sub?(objectClass=person)", self$url, paste0(people_attrs,collapse=","))
+      self$INFO(sprintf("LDAP query for persons: %s", ldap_person_url))
+      ldap_person <- RCurl::getURL(ldap_person_url)
+      self$INFO(ldap_person)
       ldap_person_dn <- unlist(strsplit(ldap_person, "DN\\: "))
       ldap_person_dn <- ldap_person_dn[ldap_person_dn!=""]
       ldap_person_dn <- paste("DN:", ldap_person_dn)
