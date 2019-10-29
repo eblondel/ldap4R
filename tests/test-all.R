f3f66448a8d9d58c15209d22a1c2db9776afec3f
eblondel <emmanuel.blondel1@gmail.com>
2019-10-29 15:56:27 +0100

    enable logger in tests

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,7 +1,12 @@
 library(testthat)
 library(ldap4R)
 
-LDAP <- LDAPClient$new(host = "localhost", port = 389, dc = "planetexpress.com")
+LDAP <- LDAPClient$new(
+  host = "localhost", 
+  port = 389, 
+  dc = "planetexpress.com", 
+  logger = "INFO"
+)
 
 if(is(LDAP, "LDAPClient")){
   cat(sprintf("LDAP server '%s' up and running. Running integration tests...\n", LDAP$url))
